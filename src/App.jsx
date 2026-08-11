5495a781bdfe0c7c8cde70d66d9baace9f018f96
manyavashistha01-create <manyavashistha01@gmail.com>
2026-08-11 18:12:24 +0530

    browerroute replace with hashroute

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import React from "react";
 import {
   Routes,
   Route,
-  BrowserRouter,
+  HashRouter,
   Outlet,
 } from "react-router-dom";
+
+
+//====================================================
+// WEBSITE COMPONENTS
+//====================================================
 
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
@@ -18,9 +23,19 @@ import Testimonials from "./Components/Testimonials/Testimonials";
 import BlogSlider from "./Components/BlogSlider/BlogSlider";
 import AppointmentForm from "./Components/AppointmentFrom/AppointmentForm";
 
-import BranchLogin from "./Components/BranchLogin/BranchLogin";
-import HomePage from "./Components/HomePage/HomePage";
 
+//====================================================
+// LOGIN
+//====================================================
+
+import BranchLogin from "./Components/BranchLogin/BranchLogin";
+
+
+//====================================================
+// CLINIC COMPONENTS
+//====================================================
+
+import HomePage from "./Components/HomePage/HomePage";
 import AddPatientForm from "./Components/AddPatientForm/AddPatientForm";
 import PrescriptionForm from "./Components/PrescriptionForm/PrescriptionForm";
 import Recharge from "./Components/Recharge/Recharge";
@@ -30,15 +45,31 @@ import ServiceCategory from "./Components/ServiceCategory/ServiceCategory";
 import ServicesSubCategory from "./Components/ServicesSubCategory/ServicesSubCategory";
 import Services from "./Components/Services/Services";
 
+
+//====================================================
+// ADMIN COMPONENTS
+//====================================================
+
 import AddClinic from "./Components/AddClinic/AddClinic";
 import PreviouslyAddedClinics from "./Components/PreviouslyAddedClinics/PreviouslyAddedClinics";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import ClinicSuspended from "./Components/Clinic-suspended/ClinicSuspended";
 
+
+//====================================================
+// SIDEBARS / LAYOUTS
+//====================================================
+
 import Sidebar1 from "./Components/Sidebar1/Sidebar1";
 import Layout from "./Layout";
+
 import ClinicSidebar from "./Components/ClinicSidebar/ClinicSidebar";
 import LayoutClinic from "./LayoutClinic";
+
+
+//====================================================
+// CLINIC SETTINGS
+//====================================================
 
 import MenuSetting from "./Components/MenuSetting/MenuSetting";
 import PatientTreatmentProtocol from "./Components/PaitentTreatmentProtocol/PatientTreatmentProtocol";
@@ -48,13 +79,18 @@ import AddDoctor from "./Components/AddDoctor/AddDoctor";
 import AddDoctorList from "./Components/AddDoctorList/AddDoctorList";
 import DashboardClinic from "./Components/DashboardClinic/DashboardClinic";
 
+
+//====================================================
+// PUBLIC PAGES
+//====================================================
+
 import BookAppointment from "./pages/BookAppointment/BookAppointment";
 import AboutUs from "./pages/AboutUs/AboutUs";
 
 
-/*====================================================
-  WEBSITE HOME CONTENT
-====================================================*/
+//====================================================
+// WEBSITE HOME CONTENT
+//====================================================
 
 function WebsiteHome() {
 
@@ -81,14 +117,14 @@ function WebsiteHome() {
 }
 
 
-/*====================================================
-  WEBSITE LAYOUT
-====================================================*/
+//====================================================
+// WEBSITE LAYOUT
+// Navbar + Page Content + Footer
+//====================================================
 
 function WebsiteLayout() {
 
   return (
-
     <>
 
       <Navbar />
@@ -98,37 +134,45 @@ function WebsiteLayout() {
       <Footer />
 
     </>
-
   );
 
 }
 
 
-/*====================================================
-  APP
-====================================================*/
+//====================================================
+// MAIN APP
+//====================================================
 
 function App() {
 
   return (
 
-    <BrowserRouter>
+    <HashRouter>
 
       <Routes>
 
 
         {/*================================================
-          PUBLIC WEBSITE
+          PUBLIC WEBSITE ROUTES
         =================================================*/}
 
         <Route element={<WebsiteLayout />}>
 
+
           {/* HOME */}
 
-          <Route path="/" element={<WebsiteHome />}/>
-           {/* about os */}
-           <Route path="/about-us" element={<AboutUs />} />
-          
+          <Route
+            path="/"
+            element={<WebsiteHome />}
+          />
+
+
+          {/* ABOUT US */}
+
+          <Route
+            path="/about-us"
+            element={<AboutUs />}
+          />
 
 
           {/* BOOK APPOINTMENT */}
@@ -138,11 +182,12 @@ function App() {
             element={<BookAppointment />}
           />
 
+
         </Route>
 
 
         {/*================================================
-          LOGIN
+          LOGIN ROUTE
         =================================================*/}
 
         <Route
@@ -157,85 +202,134 @@ function App() {
 
         <Route element={<LayoutClinic />}>
 
+
+          {/* CLINIC DASHBOARD */}
+
           <Route
             path="/dashboard-clinic"
             element={<DashboardClinic />}
           />
+
+
+          {/* HOME PAGE */}
 
           <Route
             path="/homepage"
             element={<HomePage />}
           />
 
+
+          {/* ADD PATIENT */}
+
           <Route
             path="/addpatient"
             element={<AddPatientForm />}
           />
+
+
+          {/* SERVICES */}
 
           <Route
             path="/services"
             element={<Services />}
           />
 
+
+          {/* SERVICE CATEGORY */}
+
           <Route
             path="/category"
             element={<ServiceCategory />}
           />
+
+
+          {/* SERVICE SUB CATEGORY */}
 
           <Route
             path="/sub-category"
             element={<ServicesSubCategory />}
           />
 
+
+          {/* CLINIC SETTINGS */}
+
           <Route
             path="/setting-clinics"
             element={<PrescriptionSettingForm />}
           />
+
+
+          {/* ADD DOCTOR */}
 
           <Route
             path="/add-doctor"
             element={<AddDoctor />}
           />
 
+
+          {/* DOCTOR LIST */}
+
           <Route
             path="/add-doctor-list"
             element={<AddDoctorList />}
           />
+
+
+          {/* MENU SETTINGS */}
 
           <Route
             path="/menu-settings"
             element={<MenuSetting />}
           />
 
+
+          {/* RECHARGE HISTORY */}
+
           <Route
             path="/recharge-history"
             element={<RechargeHistory />}
           />
+
+
+          {/* RECHARGE */}
 
           <Route
             path="/recharge"
             element={<Recharge />}
           />
 
+
+          {/* OPEN PATIENT LIST */}
+
           <Route
             path="/openpatientlist"
             element={<OpenPatientList />}
           />
+
+
+          {/* ATTENDANCE */}
 
           <Route
             path="/attendance"
             element={<PatientAttendance />}
           />
 
+
+          {/* TREATMENT PROTOCOL */}
+
           <Route
             path="/treatment-protocol"
             element={<PatientTreatmentProtocol />}
           />
 
+
+          {/* PRESCRIPTION */}
+
           <Route
             path="/prescription"
             element={<PrescriptionForm />}
           />
+
 
         </Route>
 
@@ -246,25 +340,38 @@ function App() {
 
         <Route element={<Layout />}>
 
+
+          {/* ADD CLINIC */}
+
           <Route
             path="/add-clinic"
             element={<AddClinic />}
           />
+
+
+          {/* RUNNING CLINICS */}
 
           <Route
             path="/running-clinic"
             element={<PreviouslyAddedClinics />}
           />
 
+
+          {/* ADMIN DASHBOARD */}
+
           <Route
             path="/dashboard-admin"
             element={<Dashboard />}
           />
 
+
+          {/* SUSPENDED CLINIC */}
+
           <Route
             path="/suspended-clinic"
             element={<ClinicSuspended />}
           />
+
 
         </Route>
 
@@ -273,22 +380,30 @@ function App() {
           OTHER ROUTES
         =================================================*/}
 
+
+        {/* ADMIN SIDEBAR */}
+
         <Route
           path="/sidebar-admin"
           element={<Sidebar1 />}
         />
+
+
+        {/* CLINIC SIDEBAR */}
 
         <Route
           path="/sidebar-clinic"
           element={<ClinicSidebar />}
         />
 
+
       </Routes>
 
-    </BrowserRouter>
+    </HashRouter>
 
   );
 
 }
+
 
 export default App;
